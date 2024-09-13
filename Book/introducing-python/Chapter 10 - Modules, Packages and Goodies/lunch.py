96a6286a0744ab692ab0bb357318a1f13134423a
SK040459 <SK040459@cerner.net>
2024-09-13 15:19:11 +0530

    first commit

---
+++ Book/introducing-python/Chapter 10 - Modules, Packages and Goodies/lunch.py
@@ -0,0 +1,5 @@
+from pydoc import plain
+import fast
+
+place = fast.pick()
+print("Let's go to", place)
---
+++ Book/introducing-python/Chapter 10 - Modules, Packages and Goodies/sources/advice.py
@@ -0,0 +1,6 @@
+from random import choice
+
+answers = ["Yes!", "No!", "Reply hazy", "Sorry, what?"]
+
+def give():
+    return choice(answers)
